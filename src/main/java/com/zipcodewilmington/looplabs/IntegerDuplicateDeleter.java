362b81711e6296b69980c889fd7486cd5cab651c
Jay <73120876+JarrellW@users.noreply.github.com>
2020-11-30 18:01:13 -0500

    completed tests

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,77 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] array){
+        super(array);
+    }
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        ArrayList<Integer> duplicateNumber = new ArrayList<>();
+        Collections.addAll(duplicateNumber, array);
+        int count = 1;
+        for (int i = 0; i < duplicateNumber.size()-1; i++) {
+            if(duplicateNumber.get(i) == duplicateNumber.get(i+1)){
+                count++;
+                if(i == duplicateNumber.size()-2){
+                    if (count >= maxNumberOfDuplications){
+                        for (int j = 0; j < count; j++) {
+                            duplicateNumber.remove(duplicateNumber.get(i+2 - count));
+                        }
+                    }
+                }
+            }else{
+                if (count >= maxNumberOfDuplications){
+                    for (int j = 0; j < count; j++) {
+                        duplicateNumber.remove(duplicateNumber.get(i+1 - count));
+                    }
+                    i = i- count;
+                }
+                count = 1;
+            }
+
+        }
+        Integer[] result = duplicateNumber.toArray(new Integer[duplicateNumber.size()]);
+
+        return result;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        ArrayList<Integer> duplicateNumbers = new ArrayList<>();
+        Collections.addAll(duplicateNumbers, array);
+        int count = 1;
+        for (int i = 0; i < duplicateNumbers.size()-1; i++) {
+            if(duplicateNumbers.get(i) == duplicateNumbers.get(i+1)){
+                count++;
+                if(i == duplicateNumbers.size()-2){
+                    if (count == exactNumberOfDuplications){
+                        for (int j = 0; j < exactNumberOfDuplications; j++) {
+                            duplicateNumbers.remove(duplicateNumbers.get(i+2 - count));
+                        }
+                    }
+                }
+            }else{
+                if (count == exactNumberOfDuplications){
+                    for (int j = 0; j < exactNumberOfDuplications; j++) {
+                        duplicateNumbers.remove(duplicateNumbers.get(i+1 - count));
+                    }
+                    i = i- count;
+                }
+
+                count = 1;
+            }
+
+        }
+        Integer[] result = duplicateNumbers.toArray(new Integer[duplicateNumbers.size()]);
+
+        return result;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,77 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
-}
+
+    public StringDuplicateDeleter(String[] array){
+        super(array);
+    }
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        ArrayList<String> duplicateString = new ArrayList<>();
+        Collections.addAll(duplicateString, array);
+        int count = 1;
+        for (int i = 0; i < duplicateString.size()-1; i++) {
+            if(duplicateString.get(i) == duplicateString.get(i+1)){
+                count++;
+                if(i == duplicateString.size()-2){
+                    if (count >= maxNumberOfDuplications){
+                        for (int j = 0; j < count; j++) {
+                            duplicateString.remove(duplicateString.get(i+2 - count));
+                        }
+                    }
+                }
+            }else{
+                if (count >= maxNumberOfDuplications){
+                    for (int j = 0; j < count; j++) {
+                        duplicateString.remove(duplicateString.get(i+1 - count));
+                    }
+                    i = i- count;
+                }
+                count = 1;
+            }
+
+        }
+        String[] result = duplicateString.toArray(new String[duplicateString.size()]);
+
+        return result;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        ArrayList<String> duplicateStrings = new ArrayList<>();
+        Collections.addAll(duplicateStrings, array);
+        int count = 1;
+        for (int i = 0; i < duplicateStrings.size()-1; i++) {
+            if(duplicateStrings.get(i) == duplicateStrings.get(i+1)){
+                count++;
+                if(i == duplicateStrings.size()-2){
+                    if (count == exactNumberOfDuplications){
+                        for (int j = 0; j < exactNumberOfDuplications; j++) {
+                            duplicateStrings.remove(duplicateStrings.get(i+2 - count));
+                        }
+                    }
+                }
+            }else{
+                if (count == exactNumberOfDuplications){
+                    for (int j = 0; j < exactNumberOfDuplications; j++) {
+                        duplicateStrings.remove(duplicateStrings.get(i+1 - count));
+                    }
+                    i = i- count;
+                }
+
+                count = 1;
+            }
+
+        }
+        String[] result = duplicateStrings.toArray(new String[duplicateStrings.size()]);
+
+        return result;
+    }
+    }
